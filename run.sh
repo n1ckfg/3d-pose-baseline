96920952121caf1cbfd595ead8f21060ed03a26b
n1ckfg <nick@fox-gieg.com>
2018-06-07 00:16:33 -0400

    test output script

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-JSON_DIR=$1
+JSON_DIR=../openpose/record
 
-python src/openpose_3dpose_sandbox.py --camera_frame --residual --batch_norm --dropout 0.5 --max_norm --evaluateActionWise --use_sh --epochs 200 --load 4874200 --openpose $JSON_DIR --gif_fps 30 --verbose 3
+python src/openpose_3dpose_output.py --camera_frame --residual --batch_norm --dropout 0.5 --max_norm --evaluateActionWise --use_sh --epochs 200 --load 4874200 --openpose $JSON_DIR --gif_fps 30 --verbose 3
